3bc1980a3d634eaf7f6e55f9ffe4ba585fa0dfaa
Ignat Drozdov <idrozdov@beringresearch.com>
2020-04-21 18:50:05 +0100

    update constants

--- constants/constants.py
+++ constants/constants.py
@@ -10,10 +10,14 @@ ENLARGED_CARDIOMEDIASTINUM = "Enlarged Cardiomediastinum"
 SUPPORT_DEVICES = "Support Devices"
 NO_FINDING = "No Finding"
 OBSERVATION = "observation"
-CATEGORIES = ["No Finding", "Enlarged Cardiomediastinum", "Cardiomegaly",
-              "Lung Lesion", "Lung Opacity", "Edema", "Consolidation",
-              "Pneumonia", "Atelectasis", "Pneumothorax", "Pleural Effusion",
-              "Pleural Other", "Fracture", "Support Devices"]
+CATEGORIES = ["No Finding", "Abnormal Mediastinal Contour", "Abnormality of Cardiac Contour", "Atelectasis",
+"Calcification", "Cardiac Calcification", "Cardiomegaly", "Cavitation", "Chest Wall Deformity", "Chest Wall Mass",
+"Colonic Interposition", "Consolidation", "Diffuse Bony Abnormality", "Dislocation", "Effusion", "Emphysema",
+"External", "Focal Bony Lesion", "Fracture", "Hiatus Hernia", "Hilar Elevation/Depression", "Hilar Enlargement",
+"Increased Hilar Density", "Spport Devices", "Interstitial Opacity", "Lobar Collapse", "Loculated Effusion",
+"Mass", "Mass Lesion", "Metalwork", "Neck Mass", "Nodule", "Spinal Deformity", "Perihilar Congestion","Pneumomediastinum",
+"Pneumoperitoneum", "Pneumothorax", "Raised Hemidiaphragm", "Spinal Deformity", "Subcutaneous Emphysema", "Thickening",
+"Vascular Calcification"]
 
 # Numeric constants
 POSITIVE = 1
